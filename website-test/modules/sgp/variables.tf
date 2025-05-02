1897965448d189212b387763f3f605db273b0bdb
Jeena Pandit <69893500+jeenapandit@users.noreply.github.com>
2025-05-02 17:24:41 +0530

    sgp module

---
+++ website-test/modules/sgp/variables.tf
@@ -0,0 +1,10 @@
+variable "project" {
+  description = "The name of the current project."
+  type        = string
+  default     = "My Project"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "VPC ID"
+}
