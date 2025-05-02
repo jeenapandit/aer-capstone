ad9260a6ce18c50f250b8310cd725d513c07a54b
Jeena Pandit <69893500+jeenapandit@users.noreply.github.com>
2025-05-02 17:22:54 +0530

    elb module

---
+++ website-test/modules/elb/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  description = "The name of the current project."
+  type        = string
+  default     = "my-project"
+}
+
+variable "allow_http_id" {
+  type = string
+  description = "Security Group id that allows http."
+}
+
+variable "subnet_a_id" {
+  type = string
+}
+
+variable "subnet_b_id" {
+  type = string
+}
